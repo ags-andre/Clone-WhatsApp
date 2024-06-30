a7477bfbf475a69ee4cb01cf0bffe4197e3c3675
ANDRE GUSTAVO SANTOS <andregustavosantos@policiamilitar.sp.gov.br>
2024-06-30 20:00:39 -0300

    merge com feature api-iniciio

--- back/WhatsApp API/obj/Debug/net8.0/WhatsApp API.AssemblyInfo.cs
+++ back/WhatsApp API/obj/Debug/net8.0/WhatsApp API.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WhatsApp API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88bf4aa985e9759055301ef5bbd09a0595fa1030")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("WhatsApp API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WhatsApp API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
